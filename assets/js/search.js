2ff52bf43917f91c998aeceb81afae8c28c35e1c
Rahul0598 <Rahul0598@users.noreply.github.com>
2022-01-10 08:17:47 +0000

    deploy: 39494330d39297e8b78075f004e36fa6724bb794

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/digital-literacy-tu-devajji/assets/js/search-data.json', true);
+    request.open('GET', '/paper/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
